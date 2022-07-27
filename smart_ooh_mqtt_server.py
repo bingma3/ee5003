d3ec8793bdd43e15013ebbf7f6c96a3ce8afcaaa
bing <bing.ma3@mail.dcu.ie>
2022-07-27 08:18:45 +0100

    Merge remote-tracking branch 'origin/server' into server

--- smart_ooh_mqtt_server.py
+++ smart_ooh_mqtt_server.py
@@ -18,24 +18,26 @@ def read_file(path):
             return False
 
 
+def seed_upload(client, msg, path):
+    bit_torrent = read_file(path)
+    client.publish("smartooh_mqtt", msg.payload[:msg.payload.find(b',') + 1] + b'seedupdate#' + bit_torrent)
+
+
 def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
     if msg:
-        # byte_string = read_file("./video_repo/2022-07-01-raspios-bullseye-i386.iso.torrent")
         byte_string = b'2022-07-01-raspios-bullseye-i386.iso.torrent'
         hashcode = hashlib.sha256()
         hashcode.update(byte_string)
         print("Message received-> " + msg.topic + " " + str(msg.payload))  # Print a received msg
         if msg.payload[msg.payload.find(b',')+1:] == hashcode.digest():
-            bit_torrent = read_file("./video_repo/2022-07-01-raspios-bullseye-i386.iso.torrent")
-            client.publish("smartooh_mqtt", msg.payload[:msg.payload.find(b',')+1] + bit_torrent)
+            seed_upload(client, msg, "./video_repo/2022-07-01-raspios-bullseye-i386.iso.torrent")
 
 
 def run():
-    client = mqtt.Client("smartooh_broker")  # Create instance of client with client ID “digi_mqtt_test”
+    client = mqtt.Client("smartooh_broker")  # Create instance of client with client ID “smartooh_broker”
     client.on_connect = on_connect  # Define callback function for successful connection
     client.on_message = on_message  # Define callback function for receipt of a message
     client.connect("192.168.1.85", 1883, 60)  # Connect to (broker, port, keepalive-time)
-    # client.connect('127.0.0.1', 17300)
     client.loop_forever()  # Start networking daemon
 
 
