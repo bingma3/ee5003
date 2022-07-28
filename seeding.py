3ef1d720cdf7cca0b42cd1552d625c098e4b7e01
bing <bing.ma3@mail.dcu.ie>
2022-07-28 13:51:04 +0100

    Merge remote-tracking branch 'origin/server' into server

--- seeding.py
+++ seeding.py
@@ -1,6 +1,13 @@
-from Py1337x import Py1337x
+import libtorrent as lt
 
-leetClient = Py1337x()
 
-searchResults = leetClient.search("Silicon Valley S01E01")
-print(searchResults)
+fs = lt.file_storage()
+lt.add_files(fs, "./video_repo/temp_1.mp4")
+t = lt.create_torrent(fs)
+t.add_tracker("http://192.168.1.85/announce")
+t.set_creator("smartooh torrent")
+t.set_comment("smartooh video")
+t.set_priv(True)
+lt.set_piece_hashes(t, "./video_repo")
+with open("./video_repo/video.torrent", "wb") as f:
+    f.write(lt.bencode(t.generate()))
